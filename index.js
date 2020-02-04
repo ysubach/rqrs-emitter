e29f28c65170401555666788fdc4ab9ae64d4b41
Yuri Subach <ysubach@gmail.com>
2020-02-03 21:53:57 -0700

    fix some edge cases; more tests; more comments

--- index.js
+++ index.js
@@ -2,24 +2,38 @@
 
 const EventEmitter = require('eventemitter3');
 
+/**
+ * Creates new RqrsEmitter object instance, it uses EventEmitter
+ * as a parent
+ */
 function RqrsEmitter() {
   EventEmitter.call(this);
+  this.handlersMap = {};
 }
 
+// Build prototype
 RqrsEmitter.prototype = Object.create(EventEmitter.prototype);
 
+// Returns internal event name based on incoming event
 function rqrsEvent(event) {
   return '_rqrs_' + event;
 }
 
+// Returns random string
 function rndstr() {
   return Math.random().toString(36).substring(2, 15);
 }
 
+/**
+ * Send new request
+ * @param event - Event name
+ * @param data - Request data
+ */
 RqrsEmitter.prototype.request = async function(event, data) {
   return new Promise((function(resolve, reject) {
     const reqId = rndstr() + rndstr();
-    this.once(reqId + rqrsEvent(event), function(res) {
+    const respEvent = reqId + rqrsEvent(event);
+    this.once(respEvent, function(res) {
       resolve(res);
     });
     const r = this.emit(rqrsEvent(event), { 
@@ -27,19 +41,38 @@ RqrsEmitter.prototype.request = async function(event, data) {
       data: data 
     });
     // r = true | false -- had listeners
-    //return 'xxx';
+    if (!r) {
+      this.removeListener(respEvent);
+      reject(new Error(`no handler found for [${event}]`));
+    }
   }).bind(this));
 }
 
+/**
+ * Add request handler
+ * @param event - Event name
+ * @param handlerFn - Request handler function (must be async)
+ */
 RqrsEmitter.prototype.addRequestHandler = function(event, handlerFn) {
-  return this.on(rqrsEvent(event), async function(req) {
+  if (this.handlersMap[event]) {
+    throw new Error(`handler already exists for [${event}]`);
+  }
+  const r = this.on(rqrsEvent(event), async function(req) {
     const res = await handlerFn(req.data);
     this.emit(req.id + rqrsEvent(event), res);
   });
+  this.handlersMap[event] = true;
+  return r;
 }
 
+/**
+ * Remove request handler
+ * @param event - Event name
+ */
 RqrsEmitter.prototype.removeRequestHandler = function(event) {
-  return this.removeListener(rqrsEvent(event));
+  const r = this.removeListener(rqrsEvent(event));
+  delete this.handlersMap[event];
+  return r;
 }
 
 //
--- index.test.js
+++ index.test.js
@@ -7,9 +7,54 @@ test('new object', () => {
   expect(rre).toBeDefined();
 });
 
-test('successful call', async () => {
+test('successful call - with request', async () => {
   const rre = new RqrsEmitter();
-  rre.handler('test', async req => 'ok' );
+  rre.handler('test', async req => {
+    expect(req.a).toBe(1);
+    return 'ok';
+  });
   const res = await rre.request('test', { a: 1 });
   expect(res).toBe('ok');
+});
+
+test('successful call - empty request', async () => {
+  const rre = new RqrsEmitter();
+  rre.handler('test', async () => 'ok');
+  const res = await rre.request('test');
+  expect(res).toBe('ok');
+});
+
+test('no handler error', async () => {
+  const rre = new RqrsEmitter();
+  try {
+    await rre.request('test')
+  } catch (e) {
+    expect(e.toString()).toBe('Error: no handler found for [test]');
+  }
+});
+
+test('remove handler', async() => {
+  const rre = new RqrsEmitter();
+  rre.handler('test', async () => 'ok');
+  const res = await rre.request('test');
+  expect(res).toBe('ok');
+  
+  rre.removeHandler('test');
+  try {
+    await rre.request('test')
+    expect(null).toBe('exception not thrown');
+  } catch (e) {
+    expect(e.toString()).toBe('Error: no handler found for [test]');
+  }
+});
+
+test('handler already exists error', async () => {
+  const rre = new RqrsEmitter();
+  rre.handler('test', async () => 'ok');
+  try {
+    rre.handler('test', async () => 'ok');
+    expect(null).toBe('exception not thrown');
+  } catch (e) {
+    expect(e.toString()).toBe('Error: handler already exists for [test]');
+  }
 });
